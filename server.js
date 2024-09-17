c98f881e9bffac3b302fdba1ca722b01dffa804d
skyepaper <111369963+skyepaper@users.noreply.github.com>
2024-09-17 15:35:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,50 +11,76 @@ app.use(cors());
 
 server = require('http').createServer(app);
 
-mongoose.connect('--hidden--', {
+mongoose.connect('mongodb+srv://borismirevbm:2YacEBc3qgz4OiLJ@aquarium.6ud9dig.mongodb.net/workstation?retryWrites=true&w=majority', {
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>console.log('Connected to DB'))
   .catch(console.error);
 
-const User=require('./models/User');
+  const Project = require('./models/Project');
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-  'force new connection': true 
-});
-server.listen(3002);
-
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-
-  socket.on('disconnect', function () {
-    console.log(`User DisConnected: ${socket.id}`);
-});
-
-  
-const UserEventEmitter = User.watch();
-UserEventEmitter.on('change', change => {
-  let text='user';
-    socket.emit('message',{text});
-});
-  
-});
-
-app.get('/users', cors(), async(req,res)=>{  
-
-  const users=await User.find();
-  res.json(users);
-});
-
-app.post('/user/new', async (req,res)=>{
-
-  const user=new User({
-      nickname:req.body.nickname,
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      //"http://localhost:3000",
+      methods: ["GET", "POST"],
+    },
+    'force new connection': true 
   });
-  user.save();
-  res.json(user);
+  server.listen(3002);
+
+
+  io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+  
+    socket.on('disconnect', function () {
+      console.log(`User DisConnected: ${socket.id}`);
+  });
+  
+    
+    const ProjectEventEmitter = Project.watch();
+    ProjectEventEmitter.on('change', change => {
+      let text='project';
+        socket.emit('message',{text});
+    });
+    
+  });
+
+
+
+
+app.get('/projects', cors(), async(req,res)=>{  
+
+    const projects = await Project.find();
+    res.json(projects);
+});
+
+app.post('/project/new', async (req,res)=>{
+
+    const project = new Project({
+     
+      name: req.body.name,
+      pass: req.body.pass,
+      type: req.body.type
+
+    });
+    project.save();
+    res.json(project);
+});
+
+app.delete('/project/delete/:id', async (req,res)=>{
+  const result=await Project.findByIdAndDelete(req.params.id);
+  res.json(result);
+})
+
+app.put('/project/save/:id', async (req,res)=>{
+
+  const project=await Project.findByIdAndUpdate(req.params.id);
+  if(project) {
+      project.name= req.body.name
+      project.pass= req.body.pass
+  }
+  
+    project.save();
+    res.json(project);
 });
