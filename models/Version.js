15efaa6780ba1d3c069d16463335b434d097fad6
skyepaper <111369963+skyepaper@users.noreply.github.com>
2024-09-18 16:44:11 +0300

    Create Version.js

---
+++ models/Version.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const VersionSchema = new Schema({
+    
+    number:{
+        type:String,
+        default:''
+    },
+    version:{
+        type:String,
+        default:''
+    },
+    description:{
+        type:String,
+        default:''
+    },
+    code:{
+        type:String,
+        default:''
+    }
+  
+})
+
+const Version = mongoose.model("Version", VersionSchema);
+module.exports = Version;
